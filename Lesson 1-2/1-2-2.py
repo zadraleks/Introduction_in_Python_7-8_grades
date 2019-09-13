46639cc6acd684851caa1c54705c3c46d8d83277
Alexander Sidorenko <alexander.sidorenko@eosda.com>
2019-09-13 17:28:10 +0300

    Lessons 1-2, 1-3

---
+++ Lesson 1-2/1-2-2.py
@@ -0,0 +1,10 @@
+#Заказчик решил добавить немного разнообразия в свои строки и дал вам следующее задание: выведите строку "Learn Python:
+#action in progress..." 3 раза, а затем поменяйте сообщение на "Learn Python: action completed." и выведите его 2 раза.
+
+a = 'Learn Python: action in progress...'
+b = 'Learn Python: action completed.'
+print(a)
+print(a)
+print(a)
+print(b)
+print(b)
